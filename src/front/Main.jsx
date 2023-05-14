c78de351f21118f44d102cf0acef1d3c9dd9c7a4
Vadim <60597212+va-kolesov@users.noreply.github.com>
2023-05-14 20:31:23 +0300

    images

--- src/front/Main.jsx
+++ src/front/Main.jsx
@@ -6,7 +6,6 @@ import "./Main.css";
 function Main() {
     return (
         <div className="Page_root Main">
-            <div className="wraper">
                 <div className="index_header">
                 <div className="Main_header_block">
                     <span className="Main_header_text">Что необходимо сделать?</span>
@@ -15,28 +14,24 @@ function Main() {
                 <div className="index_middle">
                
                     <Link to="/encrypt" style={{ textDecoration: 'none' }}>
-                        <button className="button1"  >
+                        <button className="button_main"  >
                             <span className="index_middle_text">Зашифровать</span>
                         </button>
                     </Link>
                     <Link to="/decoding" style={{ textDecoration: 'none' }}>
-                        <button className="button2" >
+                        <button className="button_main" >
                             <span className="index_middle_text">Расшифровать</span>
                         </button>
                     </Link>
  
                 </div>
-                <div className="flex-row Page_body">
+                <div className="flex-row">
                 <span>   
                 </span>
                  <div className="stenog">
                       <span className="text3">Стеганография в BPM</span>
                     </div>
                 </div>
-                  
-                    
-                
-            </div>
         </div>
     );
 }
